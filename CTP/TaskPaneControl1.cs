929788d8c564a87d6a4e15d5c72472dd904616c3
Devagnanam Jayaseelan (DEVA) <djayasee@microsoft.com>
2019-02-05 10:42:45 -0800

    Add project files.

---
+++ CTP/TaskPaneControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTP
+{
+    public partial class TaskPaneControl1 : UserControl
+    {
+        public TaskPaneControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CTP/ThisAddIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools;
+
+namespace CTP
+{
+    public partial class ThisAddIn
+    {
+        private TaskPaneControl1 myUserControl1;
+        private Microsoft.Office.Tools.CustomTaskPane myCustomTaskPane;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            myUserControl1 = new TaskPaneControl1();
+            myCustomTaskPane = this.CustomTaskPanes.Add(myUserControl1, "My Task Pane");
+
+            myCustomTaskPane.Visible = true;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
